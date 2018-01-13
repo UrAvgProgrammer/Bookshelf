49d8c8c12938b357513de4c5e6634335929fceef
LanceSanchez <31213346+LanceSanchez@users.noreply.github.com>
2018-01-13 15:57:22 +0800

    Add files via upload
    Final Project

--- bookshelf/app/config.py
+++ bookshelf/app/config.py
@@ -1,4 +1,4 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:user@localhost/bookshelf'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:sw33t890@localhost/bookshelf'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'this is it pancit'
